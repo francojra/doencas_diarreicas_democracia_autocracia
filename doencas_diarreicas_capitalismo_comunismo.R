e80f1a7aa0b9ad6c9f8118c196d146dff01a011e
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-09 23:15:37 -0300

    Add files via upload

---
+++ doencas_diarreicas_capitalismo_comunismo.R
@@ -0,0 +1,14 @@
+
+# Doenças diarréicas em países capitalistas e comunistas -----------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 09/10/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/diarrheal-diseases --------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Dados estimam que 1,6 milhões de pessoas morreram devido à doenças diarréicas em 2017. Doenças diarréicas
+### estão entre as principais causas de morte global. Nesse registro nós observamos o peso dessa doença
+### e o que podemos fazer para reduzir ela.
+
+
+
