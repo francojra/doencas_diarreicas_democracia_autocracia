d206a20fe20c8d5b3ed076ac05ae672ce490d5c2
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-12 22:45:39 -0300

    Add files via upload

--- doencas_diarreicas_capitalismo_comunismo.R
+++ doencas_diarreicas_capitalismo_comunismo.R
@@ -22,6 +22,7 @@
 library(tidyverse)
 library(cols4all)
 library(hrbrthemes)
+library(ggthemes)
 
 # Carregar dados ---------------------------------------------------------------------------------------------------------------------------
 
@@ -48,6 +49,10 @@ diarr1 <- diarr %>%
 diarr2 <- diarr %>%
   filter(Entity %in% c("United States", "Germany", "Japan",
                        "China", "Cuba", "North Korea")) %>%
+  view()
+
+diarr3 <- diarr %>%
+  filter(Entity %in% c("United States", "China")) %>%
   view()
 
 # Gráficos ---------------------------------------------------------------------------------------------------------------------------------
@@ -84,7 +89,19 @@ ggplot(diarr2, aes(x = Year, y = taxa_mort_diar,
   theme_ipsum(axis_title_size = 16, axis_text_size = 14) +
   theme(axis.text = element_text(color = "black"))
 
+c4a("dark2", 2)
 
+ggplot(diarr3, aes(x = Year, y = taxa_mort_diar, 
+                  group = Entity, col = Entity)) +
+  geom_line(size = 2.2) +
+  scale_color_manual(values = c("#1B9E77", "#D95F02"),
+                     labels = c("China", "Estados Unidos")) +
+  labs(x = "Tempo (anos)", y = "Taxa de mortes por doenças diarréicas", 
+       color = "Países") +
+  theme_hc() +
+  theme(axis.title = element_text(size = 18),
+        axis.text = element_text(color = "black", size = 15),
+        legend.text = element_text(size = 12))
 
 
 
